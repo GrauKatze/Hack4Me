782b8d31c8a02b8ac50c93b1a945e0acfda64340
GrauKatze <raot_rus@hotmail.com>
2021-05-23 16:42:23 +0300

    фикс

--- src/Program.java
+++ src/Program.java
@@ -1,21 +1,8 @@
-import Interfaces.IClient;
-import Interfaces.IServer;
-import Log.*;
 
 public class Program {
     // TODO: добавить связь между сервером и клиентом
     public static void main(String[] args) throws Exception {
 
-        ILogger Log = new Logger();
         
-        Server server = new Server();
-        server.start();
-        
-        Client client = new Client();
-        client.start();
-        
-        IClient clnt = client;
-        IServer srv = server;
-
     }
 }
