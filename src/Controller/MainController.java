a443a3023e53879da8727aafb533c2499796b501
GrauKatze <raot_rus@hotmail.com>
2021-05-27 19:11:46 +0300

    Controller add

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,16 +1,21 @@
-package View;
+package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
  
 public class MainController {
      
     @FXML
     private Button btn;
+
+    @FXML
+    private TextField userNick;
      
     @FXML
     private void click(ActionEvent event) {
-        btn.setText("You've clicked!");
+            btn.setText(userNick.getText());
+        }
     }
-}
