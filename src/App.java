d6400f00bc5d5424f8413a763340ed390de9fee4
GrauKatze <raot_rus@hotmail.com>
2021-06-05 00:33:37 +0300

    переписал вызов приветсвия

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import Model.Client.Client;
-import Model.Server.Server;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application{
+
     public static void main(String[] args) {
         Application.launch(args);
     }
@@ -15,29 +14,30 @@ public class App extends Application{
     public void start(Stage stage) throws Exception {
           
         Parent root = FXMLLoader.load(getClass().getResource("View/Main.fxml")); 
-        Scene scene = new Scene(root);
-         
-        stage.setScene(scene);
+
+        stage.setScene(new Scene(root));
         stage.setTitle("Hello JavaFX");
         stage.show();
     }
 
     @Override
     public void init() throws Exception {
-        Server srv = new Server();
-        Client cln = new Client();
-        srv.start();
-        cln.start();
+        // Server srv = new Server();
+        // Client cln = new Client();
+        // srv.start();
+        // cln.start();
+
+
         super.init();
     }
 
-    @Override
-    public void stop() throws Exception {
-        Server srv = new Server();
-        Client cln = new Client();
-        srv.stop();
-        cln.stop();
+    // @Override
+    // public void stop() throws Exception {
+    //     Server srv = new Server();
+    //     Client cln = new Client();
+    //     srv.ClouseServer();
+    //     cln.ClouseClient();
 
-        super.stop();
-    }
+    //     super.stop();
+    // }
 }
--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,39 +1,63 @@
 package Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import Model.Client.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
  
-public class MainController {
+public class MainController implements Initializable{
      
     @FXML
     private Button userInputBtn;
 
     @FXML
     private Label userNick;
+
+    @FXML
+    private Canvas battle;
      
     @FXML
     private void userInputBtnClick(ActionEvent event) {
-        Stage stage = new Stage();
+       
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        AddUser();
+                 
+        GraphicsContext gc = battle.getGraphicsContext2D();
+        gc.clearRect(0, 0, battle.getWidth(), battle.getHeight());
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillRect(0, 0, battle.getWidth(), battle.getHeight());
+        
+    }
+
+    private void AddUser() {
+        Stage stage = new Stage();        
+
+        VBox root = new VBox();
+        root.setPadding(new Insets(10, 10, 10, 10));
+        root.setSpacing(10);
+        Label userInputLabel = new Label("Your Name");
         TextField userInputNickTextField = new TextField();
-        AnchorPane.setTopAnchor(userInputNickTextField, 10.0);
-        AnchorPane.setLeftAnchor(userInputNickTextField, 60.0);
-        AnchorPane.setRightAnchor(userInputNickTextField, 60.0);
-      
         Button userInputNickButton = new Button("Bottom");
-        AnchorPane.setBottomAnchor(userInputNickButton, 10.0);
-        AnchorPane.setLeftAnchor(userInputNickButton, 30.0);
-        AnchorPane.setRightAnchor(userInputNickButton, 30.0);
-
 
         userInputNickButton.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
               
@@ -46,13 +70,12 @@ public class MainController {
             }
         });
 
-        AnchorPane root = new AnchorPane(userInputNickTextField, userInputNickButton);
+        root.getChildren().addAll(userInputLabel, userInputNickTextField, userInputNickButton);
         Scene scene = new Scene(root, 300, 200);
-       
 
         stage.setScene(scene);         
-        stage.setTitle("AnchorPane in JavaFX");
-        stage.show();
+        stage.setTitle("New User");
+        stage.showAndWait();
 
     }
 }
